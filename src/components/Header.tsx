4cdd43f4c3a1b18f1538788e4b01adfd2c05c1ee
Mahoro <lionmill3@gmail.com>
2025-08-29 10:26:26 +0200

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+const Header =()=>{
+
+
+
+    return (
+        <div className="h-full">
+            <div>
+                Mediconnect
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,16 @@
+
+const Homepage=()=>{
+
+    return(
+        <div className="h-full">
+            <div className="flex">
+                <h1>Mediconnect</h1>
+            </div>
+            <div>
+                Login
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
